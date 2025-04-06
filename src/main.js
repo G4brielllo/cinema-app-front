8d1e2a3116deec208b309317284bd0ff749757c4
G4brielllo <gabrieltarasiuk3@gmail.com>
2025-04-06 19:02:42 +0200

    adjust vuetify configuration

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import { createVuetify } from 'vuetify';
-import 'vuetify/styles';
+import { createApp } from 'vue'
+import App from './App.vue'
 
-const app = createApp(App);
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
-const vuetify = createVuetify();
+const vuetify = createVuetify({
+  components,
+  directives,
+})
 
-app.use(vuetify);
-
-app.mount('#app');
+createApp(App).use(vuetify).mount('#app')
